39a2521761e8db003ef0cf83145e4e736f653efb
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-06-06 01:38:59 +0400

    add the NULL checking if statement to it

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,9 +7,10 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-		return (NULL);
 	binary_tree_t *new_node = binary_tree_node(parent, value);
+
+	if (new_node == NULL)
+		return (NULL);
 
 	if (parent->left == NULL)
 	{
@@ -27,5 +28,4 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (new_node);
 	}
 	return (NULL);
-
 }
